5a8b02850098ad302bde8b5dc6ca732a9fdc4483
Aarushi Machavarapu <aarushimac@gmail.com>
2017-06-08 14:32:07 +0000

    ToDo strings final

---
+++ code.rb
@@ -0,0 +1,16 @@
+def crazy_strings(string, stringtwo)
+
+string=string.reverse.upcase
+stringtwo=stringtwo.swapcase
+
+if stringtwo.include? "s"
+    x = stringtwo("s") 
+    stringtwo(x) = "z"
+    
+    
+    
+    
+end
+string
+end
+
